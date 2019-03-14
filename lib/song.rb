3ca096eb121602a891feb41f035d9864293c1c57
Myrto Konstantinidi <myrtokonst@gmail.com>
2019-03-14 09:11:14 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,21 @@ def Song
  attr_accessor :name, :artist, :genre 
  
  @@count = 0
+ @@genres =[]
  def initialize (name, artist, genre)
    @name = name
    @artist = artist
    @genre = genre
    @@count +=1
-end 
+   @@genres << genre
+ end 
+ 
+ def self.count 
+   @@count
+ end 
+ 
+ def self.genres 
+   @@genres.uniq
+end
+
+ end 
